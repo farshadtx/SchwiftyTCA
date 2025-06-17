b81f3713c0903c4add1a824d81d25126e44522fc
Farshad Sheykhi <farshad@swiftgraph.dev>
2025-06-17 12:29:51 -0700

    Initial Commit

---
+++ SchwiftyTCA/SchwiftyTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchwiftyTCAApp.swift
+//  SchwiftyTCA
+//
+//  Created by Farshad Sheykhi on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SchwiftyTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
